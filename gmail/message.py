74e669c0eb267ee1480a62ab5be92b24aeddd4df
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2014-03-21 11:54:43 +0100

    fix a bug when content of attachment is None (strange bug with email forward)

--- gmail/message.py
+++ gmail/message.py
@@ -220,7 +220,10 @@ class Attachment:
         # Raw file data
         self.payload = attachment.get_payload(decode=True)
         # Filesize in kilobytes
-        self.size = int(round(len(self.payload)/1000.0))
+        try:
+            self.size = int(round(len(self.payload) / 1000.0))
+        except TypeError:
+            self.size = 0
 
     def save(self, path=None):
         if path is None:
