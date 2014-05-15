fcf8d743985853d2b343406be3ef59f97b9c6590
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-05-15 19:29:50 +0500

    Clean Comments

--- gmail/exceptions.py
+++ gmail/exceptions.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
 """
-gmail.exceptions
-~~~~~~~~~~~~~~~~~~~
-
 This module contains the set of Gmails' exceptions.
-
 """
-
-
 class GmailException(RuntimeError):
-    """There was an ambiguous exception that occurred while handling your
-    request."""
+    """
+    There was an ambiguous exception that occurred while handling your
+    request.
+    """
 
 class ConnectionError(GmailException):
-    """A Connection error occurred."""
+    """
+    A Connection error occurred.
+    """
 
 class AuthenticationError(GmailException):
-    """Gmail Authentication failed."""
+    """
+    Gmail Authentication failed.
+    """
 
 class Timeout(GmailException):
-    """The request timed out."""
+    """
+    The request timed out.
+    """
