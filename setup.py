075587b59044087964012bd948ffab65715b6593
Wilberto Morales <wilbertomorales777@gmail.com>
2014-10-08 05:32:26 -0500

    Renames for our fork

--- setup.py
+++ setup.py
@@ -1,26 +1,17 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name = "gmail",
-    version = "0.0.5",
-    author = "Charlie Guo",
-    author_email = "FIXME",
-    description = ("A Pythonic interface for Google Mail."),
+    name = "gmail-client",
+    version = "0.0.1",
+    author = "Wilberto Morales",
+    author_email = "wilbertomorales777@gmail.com",
+    description = ("A Pythonic interface for Google Mail. Based of https://github.com/charlierguo/gmai"),
     license = "MIT",
-    keywords = "google gmail",
-    url = "https://github.com/charlierguo/gmail",
-    packages=['gmail'],
-    long_description=read('README.md'),
+    keywords = "google gmail client",
+    url = "https://github.com/wilbertom/gmail",
+    packages=['gmail_client'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
         "Topic :: Communications :: Email",
         "License :: OSI Approved :: MIT License",
     ],
